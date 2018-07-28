d5a9eb89ae0107277f73dc837c95ce12c2e5e8a6
Bryan Benson <bryanbenson83@gmail.com>
2018-07-28 15:15:49 -0400

    Add answer and user functionality

--- polls/urls.py
+++ polls/urls.py
@@ -6,6 +6,7 @@ app_name = 'polls'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:pk>/<int:user_id>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:user_id>/<int:question_id>/userIndex/', views.UserIndexView.as_view(), name='userIndex'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
